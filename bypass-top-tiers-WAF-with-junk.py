ee2c94b1819a5fedad084d99810df8702cc22ad3
David Botelho Mariano <48680041+david-botelho-mariano@users.noreply.github.com>
2024-09-29 16:00:39 -0300

    Update bypass-top-tiers-WAF-with-junk.py

--- bypass-top-tiers-WAF-with-junk.py
+++ bypass-top-tiers-WAF-with-junk.py
@@ -1,37 +1,18 @@
-from burp import IBurpExtender, IHttpListener
+from mitmproxy import http
 import json
-from collections import OrderedDict
 
-class BurpExtender(IBurpExtender, IHttpListener):
-
-    def registerExtenderCallbacks(self, callbacks):
-        self._callbacks = callbacks
-        self._helpers = callbacks.getHelpers()
-        self._callbacks.setExtensionName("bypass-top-tiers-WAF-with-junk")
-        self._callbacks.registerHttpListener(self)
-
-    def processHttpMessage(self, toolFlag, messageIsRequest, messageInfo):
-        if messageIsRequest:
-            request = messageInfo.getRequest()
-            analyzedRequest = self._helpers.analyzeRequest(request)
-
-            headers = analyzedRequest.getHeaders()
-            body = request[analyzedRequest.getBodyOffset():].tostring()
-
-            if 'application/json' in headers:
-                try:
-                    body_json = json.loads(body, object_pairs_hook=OrderedDict)
-
-                    # Insert the new key-value pair at the top
-                    new_body_json = OrderedDict([('ParameterNameThatDoesNotExist', '0' * 4600)] + body_json.items()) # 132.000 characters
-
-                    # Convert JSON back to a string and update the request
-                    new_body = json.dumps(new_body_json)
-                    new_request = self._helpers.buildHttpMessage(headers, new_body)
-                    messageInfo.setRequest(new_request)
-
-                except Exception as e:
-                    # If the body is not valid JSON or another error occurs, skip processing
-                    print("Error processing JSON: {}".format(e))
-                    return
-
+def request(flow: http.HTTPFlow) -> None:
+    # Only process requests with a JSON content-type
+    content_type = flow.request.headers.get("Content-Type", "")
+    if "application/json" in content_type:
+        # Decode the JSON body
+        try:
+            data = json.loads(flow.request.get_text())
+        except json.JSONDecodeError:
+            return  # Not valid JSON, skip processing
+        
+        # Add the "x-secret" parameter
+        data["x-secret"] = "123456789"
+        
+        # Encode the JSON and set it as the new request body
+        flow.request.set_text(json.dumps(data))
